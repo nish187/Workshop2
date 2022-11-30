553ec25c07b4e7848914c7c4a17a393296a162a3
nisha <nishantsingh187@gmail.com>
2022-11-30 19:35:58 +0530

    Workshop2

---
+++ src/TwosArrays.java
@@ -0,0 +1,19 @@
+public class TwosArrays
+{
+    public static void main(String[] args) {
+        int arr[]={4,8,7,3,1};
+        int n=arr.length;
+
+            for(int i=0;i<n;i++)
+            {if(arr[i]%2==0) {
+                System.out.println("true" + arr[i]);
+            }
+
+                else
+                {
+                    System.out.println("False");
+                }
+            }
+        }
+    }
+
